dc255b005f9bd7425fbb0048b67b39f14a7b0e52
Sava Danchev <sdanchev4090@stu.d214.org>
2022-04-04 14:07:41 -0500

    Initial Commit

---
+++ GitHubPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubPractice
+//
+//  Created by Sava Danchev on 4/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
