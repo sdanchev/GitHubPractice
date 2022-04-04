8d83229c2efe304c62bc56874a8cd082a0663e9e
Sava Danchev <sdanchev4090@stu.d214.org>
2022-04-04 14:32:54 -0500

    ViewController

--- GitHubPractice/ViewController.swift
+++ GitHubPractice/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNumberTextField: UITextField!
+    @IBOutlet weak var secondNumberTextField: UITextField!
+    @IBOutlet weak var operationLabel: UITextField!
+    @IBOutlet weak var answerLabel: UITextField!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func whenAddButtonPressed(_ sender: UIButton) {
+        
+    }
+    
 }
 
